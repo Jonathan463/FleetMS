5ead072f7fad5b40be7a2e4494bdf0f89cdf32e1
Decagon <decagon@Decagons-MacBook-Air-Jonathan.local>
2021-12-04 02:09:11 +0100

    My first Commit

---
+++ src/main/java/com/example/fleetmvp_v2/FleetMvpV2Application.java
@@ -0,0 +1,13 @@
+package com.example.fleetmvp_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FleetMvpV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FleetMvpV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fleetmvp_v2/FleetMvpV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fleetmvp_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FleetMvpV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
